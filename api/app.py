0fdd32264f6a51f24a920a56a82a4999bcd0f82a
Lincoln Nguyen <137611486+rinkaaan@users.noreply.github.com>
2024-03-18 06:17:37 -0700

    Add multi-user transcriptions

--- api/app.py
+++ api/app.py
@@ -3,8 +3,9 @@ import time
 from apiflask import APIFlask, HTTPBasicAuth
 from b2sdk.v2 import InMemoryAccountInfo
 from dotenv import load_dotenv
+from flask import session
 from flask_cors import CORS
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room, emit, leave_room
 
 load_dotenv()
 
@@ -20,9 +21,61 @@ app.config["SYNC_LOCAL_SPEC"] = True
 CORS(app, supports_credentials=False, origins="*", allow_headers="*", expose_headers="*")
 
 
-@socketio.on("connect")
-def on_connect():
-    print("Client connected!")
+class SocketTranscriptionPayload:
+    def __init__(self, username: str, room: str, text: str, translation: str):
+        self.username = username
+        self.room = room
+        self.text = text
+        self.translation = translation
+
+
+class SocketJoinLeavePayload:
+    def __init__(self, username: str, room: str):
+        self.username = username
+        self.room = room
+
+
+class SocketMessagePayload:
+    def __init__(self, username: str, room: str, text: str):
+        self.username = username
+        self.room = room
+        self.text = text
+
+
+@socketio.on("join")
+def on_join(data):
+    data = SocketJoinLeavePayload(**data)
+    session["username"] = data.username
+    session["room"] = data.room
+    join_room(data.room)
+    payload = SocketMessagePayload("Server", data.room, f"{data.username} has joined the room")
+    emit("message", vars(payload), to=data.room)
+
+
+@socketio.on("disconnect")
+def on_disconnect():
+    payload = SocketMessagePayload("Server", session["room"], f"{session["username"]} has left the room")
+    emit("message", vars(payload), to=session["room"])
+
+
+@socketio.on("transcription")
+def on_transcription(data):
+    data = SocketTranscriptionPayload(**data)
+    emit("transcription", vars(data), to=data.room)
+
+
+@socketio.on("message")
+def on_message(data):
+    data = SocketMessagePayload(**data)
+    emit("message", vars(data), to=data.room)
+
+
+@socketio.on("leave")
+def on_leave(data):
+    data = SocketJoinLeavePayload(**data)
+    leave_room(data.room)
+    payload = SocketMessagePayload("Server", data.room, f"{data.username} has left the room")
+    emit("message", vars(payload), to=data.room)
 
 
 @app.get("/ping")
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,7 @@
 #(cd api && flask run --host=0.0.0.0 --port $PORT)
 #(cd .. && PYTHONPATH=. python api/app.py)
 #(cd .. && export PORT=34201 PYTHONPATH=. && gunicorn -w 1 'api.app:app' -b 0.0.0.0:$PORT)
-(export PYTHONPATH=. && gunicorn -w 1 'api.app:app' -b 0.0.0.0:$PORT)
+#(export PYTHONPATH=. && gunicorn -w 1 'api.app:app' -b 0.0.0.0:$PORT)
+#(export PYTHONPATH=. && flask run --host=0.0.0.0 --port $PORT)
+(export PYTHONPATH=. && flask --app api.app run --host=0.0.0.0 --port $PORT --no-reload)
 
