870bfd03d3fddaa5cd678d219ee8b2849ab527dd
Lincoln Nguyen <137611486+rinkaaan@users.noreply.github.com>
2024-03-19 00:39:52 -0700

    Add marshmallow schemas to PingOut

--- api/app.py
+++ api/app.py
@@ -1,11 +1,13 @@
 import time
 
-from apiflask import APIFlask, HTTPBasicAuth
+from apiflask import APIFlask, HTTPBasicAuth, Schema
+from apiflask.fields import String, Nested
 from b2sdk.v2 import InMemoryAccountInfo
 from dotenv import load_dotenv
 from flask import session
 from flask_cors import CORS
 from flask_socketio import SocketIO, join_room, emit, leave_room
+from marshmallow import post_load
 
 load_dotenv()
 
@@ -21,66 +23,92 @@ app.config["SYNC_LOCAL_SPEC"] = True
 CORS(app, supports_credentials=False, origins="*", allow_headers="*", expose_headers="*")
 
 
-class SocketTranscriptionPayload:
-    def __init__(self, username: str, room: str, text: str, translation: str):
-        self.username = username
-        self.room = room
-        self.text = text
-        self.translation = translation
+class BaseSchema(Schema):
+    @post_load
+    def make_object(self, data, **_kwargs):
+        class_name = self.__class__.__name__
+        return type(class_name, (object,), data)()
 
 
-class SocketJoinLeavePayload:
-    def __init__(self, username: str, room: str):
-        self.username = username
-        self.room = room
+class SocketTranscriptionPayload(BaseSchema):
+    username = String()
+    room = String()
+    text = String()
+    translation = String()
 
 
-class SocketMessagePayload:
-    def __init__(self, username: str, room: str, text: str):
-        self.username = username
-        self.room = room
-        self.text = text
+class SocketJoinLeavePayload(BaseSchema):
+    username = String()
+    room = String()
+
+
+class SocketMessagePayload(BaseSchema):
+    username = String()
+    room = String()
+    text = String()
 
 
 @socketio.on("join")
-def on_join(data):
-    data = SocketJoinLeavePayload(**data)
-    session["username"] = data.username
-    session["room"] = data.room
-    join_room(data.room)
-    payload = SocketMessagePayload("Server", data.room, f"{data.username} has joined the room")
-    emit("message", vars(payload), to=data.room)
+def on_join(payload):
+    payload = SocketJoinLeavePayload().load(payload)
+    session["username"] = payload.username
+    session["room"] = payload.room
+    join_room(payload.room)
+    response = {
+        "username": "Server",
+        "room": payload.room,
+        "text": f"{payload.username} has joined the room"
+    }
+    emit("message", response, to=payload.room)
 
 
 @socketio.on("disconnect")
 def on_disconnect():
-    payload = SocketMessagePayload("Server", session["room"], f"{session["username"]} has left the room")
-    emit("message", vars(payload), to=session["room"])
+    if "username" not in session:
+        return
+    response = {
+        "username": "Server",
+        "room": session["room"],
+        "text": f"{session['username']} has left the room"
+    }
+    emit("message", response, to=session["room"])
 
 
 @socketio.on("transcription")
-def on_transcription(data):
-    data = SocketTranscriptionPayload(**data)
-    emit("transcription", vars(data), to=data.room)
+def on_transcription(payload):
+    emit("transcription", payload, to=payload["room"])
 
 
 @socketio.on("message")
-def on_message(data):
-    data = SocketMessagePayload(**data)
-    emit("message", vars(data), to=data.room)
+def on_message(payload):
+    emit("message", payload, to=payload["room"])
 
 
 @socketio.on("leave")
-def on_leave(data):
-    data = SocketJoinLeavePayload(**data)
-    leave_room(data.room)
-    payload = SocketMessagePayload("Server", data.room, f"{data.username} has left the room")
-    emit("message", vars(payload), to=data.room)
+def on_leave(payload):
+    payload = SocketJoinLeavePayload().load(payload)
+    leave_room(payload.room)
+    response = {
+        "username": "Server",
+        "room": payload.room,
+        "text": f"{payload.username} has left the room"
+    }
+    emit("message", response, to=payload.room)
+
+
+class PingOut(Schema):
+    socket_transcription_payload = Nested(SocketTranscriptionPayload)
+    socket_join_leave_payload = Nested(SocketJoinLeavePayload)
+    socket_message_payload = Nested(SocketMessagePayload)
+    message = String()
 
 
 @app.get("/ping")
+@app.output(PingOut)
 def ping():
-    return "pong"
+    return {
+        "message": "pong"
+    }
 
 
 @app.before_request
